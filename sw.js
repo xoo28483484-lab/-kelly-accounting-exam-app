332e0c2ca1275e1acc5f37c31c9c5f1d1ad21396
xoo28483484-lab <xoo28483484@gmail.com>
2026-09-22 16:44:44 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='kelly-accounting-pwa-v1';
+const ASSETS=['./','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
+});
